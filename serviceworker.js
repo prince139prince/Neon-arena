896d370e608119b145c5e781db7c89a3536bcaac
prince139prince <prince139prince@gmail.com>
2026-02-01 15:31:17 +0530

    Update serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'neonarena-v1';
+
+const FILES_TO_CACHE = [
+  'index.html',
+  'style.css',
+  'animations.css',
+  'responsive.css',
+  'main.js',
+  'auth.js',
+  'wallet.js',
+  'admin.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
